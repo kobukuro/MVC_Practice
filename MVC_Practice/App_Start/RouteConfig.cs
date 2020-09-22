5eb918eaa4055d854e8c6329dfb1605ae3d0cf60
Peterkt Chen 陳冠廷 (中光電) <peterkt.chen@coretronic.com>
2020-09-22 12:06:08 +0800

    加入專案檔案。

---
+++ MVC_Practice/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_Practice
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //愈specific要放在愈前面
+            routes.MapRoute(
+                name: "moviesByReleaseDate",
+                url:"movies/released/{year}/{month}",
+                defaults:new { controller = "Movies", action = "ByReleaseDate"}
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC_Practice/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Practice.Models;
+
+namespace MVC_Practice.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            //return View(movie);
+            //return Content("hello world");
+            return HttpNotFound();
+        }
+
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
